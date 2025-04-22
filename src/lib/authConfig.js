d768243b520fa903349a1ab5ed401722f88dba95
hariclerry <clerryha@gmail.com>
2025-04-21 22:54:09 -0500

    change cognito credentials

--- src/lib/authConfig.js
+++ src/lib/authConfig.js
@@ -3,11 +3,13 @@ const poolId = process.env.REACT_APP_COGNITO_USER_POOL_ID;
 const clientId = process.env.REACT_APP_COGNITO_CLIENT_ID;
 const redirectUri = process.env.REACT_APP_REDIRECT_URI;
 
+// https://us-east-1mcxc30l9k.auth.us-east-1.amazoncognito.com
 const authConfig = {
-  authority: `https://cognito-idp.${region}.amazonaws.com/${poolId}`,
+  authority: `https://cognito-idp.${region}.amazonaws.com/${poolId}`, // Fixed missing backtick`
+  // authority:  `https://us-east-1mcxc30l9k.auth.us-east-1.amazoncognito.com`, // Added missing backtick`
   client_id: clientId,
   redirect_uri: redirectUri,
-  response_type: "code",
+  response_type: "code", // Updated response type to "code"
   scope: "openid email profile",
 };
 
